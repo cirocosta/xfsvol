0d11cc307ffa3eb42ef0227eb75e3d1d93be3c0a
Ciro S. Costa <ciro.costa@liferay.com>
2017-07-18 10:00:48 -0300

    Adds travis setup

---
+++ .travis/setup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -o errexit
+
+main() {
+	create_xfs_loopback_device
+	create_testing_directory
+}
+
+create_testing_directory() {
+	echo "INFO:
+  Creating testing directory /mnt/xfs/tmp
+  "
+
+	sudo mkdir -p /mnt/xfs/tmp
+	sudo chown -R $(whoami) /mnt/xfs/tmp
+
+	echo "SUCCESS:
+  Testing directory created
+  "
+
+	tree /mnt
+}
+
+create_xfs_loopback_device() {
+	echo "INFO:
+  Creating XFS loopback device 
+  "
+
+	sudo dd if=/dev/zero of=/xfs.1G bs=1M count=1024
+	sudo losetup /dev/loop0 /xfs.1G
+	sudo mkfs -t xfs -n ftype=1 /dev/loop0
+	sudo mkdir -p /mnt/xfs
+	sudo mount /dev/loop0 /mnt/xfs -o pquota
+
+	echo "SUCCESS:
+  Device created.
+  "
+
+	lsblk
+}
+
+main
