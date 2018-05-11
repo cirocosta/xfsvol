5c0965d825fd4ba46f068530215b6187acd98462
Ciro S. Costa <ciro.costa@liferay.com>
2018-05-11 21:10:55 +0000

    Adds back hardcoded definitions to support linux 4.4

--- xfs/xfs.h
+++ xfs/xfs.h
@@ -27,6 +27,32 @@
  *      #define XFS_DQ_PROJ          0x0002
  *
  */
+
+#ifndef FS_XFLAG_PROJINHERIT
+
+struct fsxattr {
+	__u32         fsx_xflags;
+	__u32         fsx_extsize;
+	__u32         fsx_nextents;
+	__u32         fsx_projid;
+	unsigned char fsx_pad[12];
+};
+
+#define FS_XFLAG_PROJINHERIT 0x00000200
+#endif
+
+#ifndef FS_IOC_FSGETXATTR
+#define FS_IOC_FSGETXATTR _IOR('X', 31, struct fsxattr)
+#endif
+
+#ifndef FS_IOC_FSSETXATTR
+#define FS_IOC_FSSETXATTR _IOW('X', 32, struct fsxattr)
+#endif
+
+#ifndef PRJQUOTA
+#define PRJQUOTA 2
+#endif
+
 #ifndef XFS_PROJ_QUOTA
 #define XFS_PROJ_QUOTA 2
 #endif
