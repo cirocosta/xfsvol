f4fa0c64dc0b32ac38f7371f4edd1144046f5f80
Ciro S. Costa <ciro.costa@liferay.com>
2018-05-11 12:45:54 +0000

    Updates xfsvolctl commands to have better description; SF xfsvolctl

--- xfsvolctl/commands/create.go
+++ xfsvolctl/commands/create.go
@@ -10,28 +10,32 @@ var Create = cli.Command{
 	Name:  "create",
 	Usage: "Creates a volume with XFS project quota enforcement",
 	Description: `Creates a docker volume with XFS pquotas.
-    XFS project quota allows one to implement directory
-    tree quota and accounting. This allows one to specify
-    an intended size to limit the size of the content below
-    a given tree.
+   XFS project quota allows one to implement directory
+   tree quota and accounting. This allows one to specify
+   an intended size to limit the size of the content below
+   a given tree.
 
-    Examples:
+   Examples:
 
-      1. create a volume with an upper limit of 10M:
+     1. create a volume with an upper limit of 10M and no limits
+        in number of inodes:
 
-         lsblk
-         NAME   MAJ:MIN RM   SIZE RO TYPE MOUNTPOINT
-         loop0    7:0    0     2G  0 loop /mnt/xfs
+            lsblk
+            NAME   MAJ:MIN RM   SIZE RO TYPE MOUNTPOINT
+            loop0    7:0    0     2G  0 loop /mnt/xfs
 
-         mount | grep xfs
-         /dev/loop0 on /mnt/xfs type xfs (rw,relatime,attr2,inode64,prjquota)
+            mount | grep xfs
+            /dev/loop0 on /mnt/xfs type xfs (rw,relatime,attr2,inode64,prjquota)
 
-         xfsvolctl create --root /mnt/xfs --name myvol --size 10M
+            xfsvolctl create \
+                --root /mnt/xfs \
+                --name myvol \
+                --size 10M
 
-    Note:
-      -  In order to have the creation functioning you must
-         first have a mount point in the filesystem that
-         is mounted on top of XFS and has 'pquota' set.
+   Note:
+     In order to have the creation functioning you must first have a
+     mount point in the filesystem that is mounted on top of XFS and
+     has 'pquota' set.
     `,
 	Flags: []cli.Flag{
 		cli.StringFlag{
@@ -40,7 +44,7 @@ var Create = cli.Command{
 		},
 		cli.StringFlag{
 			Name:  "size, s",
-			Usage: "Size of the XFS project quota to apply",
+			Usage: "Size of the XFS project quota to apply (e.g.: 50M)",
 		},
 		cli.Uint64Flag{
 			Name:  "inode, i",
@@ -48,53 +52,56 @@ var Create = cli.Command{
 		},
 		cli.StringFlag{
 			Name:  "root, r",
-			Usage: "Root of the volume creation",
+			Usage: "Root of the volume creation (under an xfs filesystem)",
 		},
 	},
-	Action: func(c *cli.Context) (err error) {
-		var (
-			name  = c.String("name")
-			size  = c.String("size")
-			root  = c.String("root")
-			inode = c.Uint64("inode")
+	Action: createAction,
+}
 
-			sizeInBytes uint64
-		)
+func createAction(c *cli.Context) (err error) {
+	var (
+		name  = c.String("name")
+		size  = c.String("size")
+		root  = c.String("root")
+		inode = c.Uint64("inode")
 
-		if name == "" || size == "" || root == "" {
-			cli.ShowCommandHelp(c, "create")
-			err = errors.Errorf("All parameters must be set.")
-			return
-		}
+		sizeInBytes uint64
+	)
 
-		mgr, err := manager.New(manager.Config{
-			Root: root,
-		})
-		if err != nil {
-			err = errors.Wrapf(err,
-				"Couldn't initiate manager")
-			return
-		}
-
-		sizeInBytes, err = manager.FromHumanSize(size)
-		if err != nil {
-			err = errors.Wrapf(err,
-				"Size supplied by user can't be converted to uint64 bytes")
-			return
-		}
-
-		_, err = mgr.Create(manager.Volume{
-			Name:  name,
-			Size:  sizeInBytes,
-			INode: inode,
-		})
-		if err != nil {
-			err = errors.Wrapf(err,
-				"Couldn't create volume name=%s bytes=%d inode=%d",
-				name, sizeInBytes, inode)
-			return
-		}
-
+	if name == "" || size == "" || root == "" {
+		cli.ShowCommandHelp(c, "create")
+		err = errors.Errorf("Name, Size and Root are required parameters.")
 		return
-	},
+	}
+
+	mgr, err := manager.New(manager.Config{
+		Root: root,
+	})
+	if err != nil {
+		err = errors.Wrapf(err,
+			"Couldn't initiate manager")
+		return
+	}
+
+	sizeInBytes, err = manager.FromHumanSize(size)
+	if err != nil {
+		err = errors.Wrapf(err,
+			"Size supplied by user '%s' can't be converted to uint64 bytes",
+			size)
+		return
+	}
+
+	_, err = mgr.Create(manager.Volume{
+		Name:  name,
+		Size:  sizeInBytes,
+		INode: inode,
+	})
+	if err != nil {
+		err = errors.Wrapf(err,
+			"Couldn't create volume name=%s bytes=%d inode=%d",
+			name, sizeInBytes, inode)
+		return
+	}
+
+	return
 }
--- xfsvolctl/commands/ls.go
+++ xfsvolctl/commands/ls.go
@@ -14,22 +14,27 @@ var Ls = cli.Command{
 	Name:  "ls",
 	Usage: "Lists the volumes managed by 'xfsvol' plugin",
 	Description: `Lists the volumes created with XFS pquotas.
-    Retrieve a list of the volumes created by 'xfsvol' Docker
-    plugin or the 'xfsvolctl' command.
+   Retrieve a list of the volumes created by 'xfsvol' Docker
+   plugin or the 'xfsvolctl' command.
 
-    Volumes are listed by their names relative to a root as
-    well as the sizes assigned as project quota in XFS.
+   Volumes are listed by their names relative to a root as
+   well as the sizes assigned as project quota in XFS.
 
-    Examples:
+   Examples:
 
-    1. create a volume with limit of 10M and then see it in
-       the list of volumes:
+     1. create a volume with limit of 10M and then see it in
+        the list of volumes:
 
-       xfsvolctl create --root /mnt/xfs --name myvol --size 10M
-       xfsvolctl ls --root /mnt/xfs
+            xfsvolctl create \
+                --root /mnt/xfs
+                --name myvol
+                --size 10M
 
-       NAME      QUOTA
-       myvol     10M
+            xfsvolctl ls \
+                --root /mnt/xfs
+
+            NAME      QUOTA
+            myvol     10M
 	`,
 	Flags: []cli.Flag{
 		cli.StringFlag{
@@ -37,42 +42,44 @@ var Ls = cli.Command{
 			Usage: "Root of the volume listing",
 		},
 	},
-	Action: func(c *cli.Context) (err error) {
-		var root = c.String("root")
-		if root == "" {
-			cli.ShowCommandHelp(c, "ls")
-			err = errors.Errorf("All parameters must be set.")
-			return
-		}
+	Action: lsAction,
+}
 
-		mgr, err := manager.New(manager.Config{
-			Root: root,
-		})
-		if err != nil {
-			err = errors.Wrapf(err,
-				"Couldn't initiate manager")
-			return
-		}
-
-		vols, err := mgr.List()
-		if err != nil {
-			err = errors.Wrapf(err,
-				"Couldn't list volumes under root %s",
-				root)
-			return
-		}
-
-		w := new(tabwriter.Writer)
-		w.Init(os.Stdout, 0, 8, 0, '\t', 0)
-		fmt.Fprintln(w, "NAME\tBLK-QUOTA\tINODE-QUOTA\t")
-
-		for _, vol := range vols {
-			fmt.Fprintf(w, "%s\t%s\t%d\n",
-				vol.Name,
-				manager.HumanSize(vol.Size),
-				vol.INode)
-		}
-		w.Flush()
+func lsAction(c *cli.Context) (err error) {
+	var root = c.String("root")
+	if root == "" {
+		cli.ShowCommandHelp(c, "ls")
+		err = errors.Errorf("All parameters must be set.")
 		return
-	},
+	}
+
+	mgr, err := manager.New(manager.Config{
+		Root: root,
+	})
+	if err != nil {
+		err = errors.Wrapf(err,
+			"Couldn't initiate manager")
+		return
+	}
+
+	vols, err := mgr.List()
+	if err != nil {
+		err = errors.Wrapf(err,
+			"Couldn't list volumes under root %s",
+			root)
+		return
+	}
+
+	w := new(tabwriter.Writer)
+	w.Init(os.Stdout, 0, 8, 0, '\t', 0)
+	fmt.Fprintln(w, "NAME\tBLK-QUOTA\tINODE-QUOTA\t")
+
+	for _, vol := range vols {
+		fmt.Fprintf(w, "%s\t%s\t%d\n",
+			vol.Name,
+			manager.HumanSize(vol.Size),
+			vol.INode)
+	}
+	w.Flush()
+	return
 }
